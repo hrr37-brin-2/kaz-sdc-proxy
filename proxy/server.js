1ed3472234497fa17ab89fe7a9dd069fd2f748d7
Kazuki Shigemitsu <kazuki.shigemitsu@gmail.com>
2019-04-29 20:52:07 -0700

    set up for the stress test

--- proxy/server.js
+++ proxy/server.js
@@ -1,13 +1,27 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/:id', express.static(path.join(__dirname, 'public')));
+// const staticPath = `${__dirname}/../public`;
+// console.log(staticPath);
+
+// app.use('/band/:id', express.static(staticPath));
+// app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+const mediaPlayer = {
+  target: 'http://localhost:3002',
+  changeOrigin: true,
+};
+const mediaPlayerProxy = proxy(mediaPlayer);
+app.use('/:id', mediaPlayerProxy)
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
